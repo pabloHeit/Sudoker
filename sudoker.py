c81ac2decb656e9b5df4350505c7b7b237dd08f6
pabloHeit <pabloaheit@yahoo.com.ar>
2022-12-01 21:03:55 -0300

    Graphics and logic merged
    Now you can type the sudoku table and you can print that table or send the table to the sudokuSolver class to complete the sudoku

--- sudoker.py
+++ sudoker.py
@@ -1,49 +1,34 @@
 from enum import Enum
 
 class sudokuSolver:
-    def __init__(self, table):
-        self.table = table
+    def __init__(self):
+        self.table = []
         self.detailedTable = list()
         self.state = Enum('state', ['error'])
         self.changes = 0
-        print('sudokuSolver instantiated')
-        self.solveSudoku()
-        self.printBeautySudoku()
-        
-        # print(self.convertToPossibleNumbers(self.check_singleSquare(7,3)))
-        # print(self.convertToPossibleNumbers(self.check_singleSquare(6,3)))
-        # print(self.convertToPossibleNumbers(self.check_singleSquare(6,5)))
-    
-    def printBeautySudoku(self):
-        sudokuText = '['
-        separators = [2, 5]
-        for row in self.table:
-            for c, number in enumerate(row):
-                sudokuText += str(number) + ' '
-                if c in separators:
-                    sudokuText += '\t'
-            sudokuText += '\n'
-            if self.table.index(row) in separators:
-                sudokuText += '\n'
-        
-        return print(sudokuText)
-                
-            
-    def solveSudoku(self):
+        self.utilities = sudokuUtilities()
+         
+    def solveSudoku(self, table):
+        self.table = table
         print('sudokuSolver start to solve the sudoku')
         self.tableDetailer()
         print('table detailed')
         
+        iterations = 0
         while self.check_isFinished() == False:
             self.check_all_OneChance()
-      
+            iterations += 1
+            if iterations >= 81:
+                break
+        return self.table
+        
     def tableDetailer(self):
         self.detailedTable.clear()
         for r, row in enumerate(self.table):
             for c, number in enumerate(row):
                 forbiddenNumbers = self.check_singleSquare(r,c)
                 possibleNumbers = self.convertToPossibleNumbers(forbiddenNumbers)
-                squareNumber = self.convertToSquareNumber(r,c)
+                squareNumber = self.utilities.convertToSquareNumber(r,c)
                 
                 self.detailedTable.append({
                     'number': number,
@@ -66,7 +51,7 @@ class sudokuSolver:
 
     def check_Line(self, line):
         forbiddenNumbers = list()
-        for square in table[line]:
+        for square in self.table[line]:
             if(square != 0):
                 forbiddenNumbers.append(square)
         if self.check_logicError(forbiddenNumbers) == False:
@@ -76,7 +61,7 @@ class sudokuSolver:
 
     def check_Column(self, column):
         forbiddenNumbers = list()
-        for row in table:
+        for row in self.table:
             for i, square in enumerate(row):
                 if i == column and square != 0:
                     forbiddenNumbers.append(square)
@@ -103,16 +88,11 @@ class sudokuSolver:
             startingColumn = 3
         if squareNumber == 2 or squareNumber == 5 or squareNumber == 8:
             startingColumn = 6
-            
-        # print(f'squareNumber: {squareNumber}')
-        # print(f'startingRow: {startingRow}')
-        # print(f'startingColumn: {startingColumn}')    
         
         for row in range(startingRow, startingRow + 3):
             for column in range(startingColumn, startingColumn + 3):
-                if table[row][column] != 0:
-                    forbiddenNumbers.append(table[row][column])
-        # print(forbiddenNumbers)
+                if self.table[row][column] != 0:
+                    forbiddenNumbers.append(self.table[row][column])
         if self.check_logicError(forbiddenNumbers) == False:
             return forbiddenNumbers
         else:
@@ -153,7 +133,6 @@ class sudokuSolver:
             '8':0,
             '9':0
         }
-        # print(self.detailedTable)
         for square in self.detailedTable:
             if square['squareNumber'] == squareNumber:
                 for number in square['possibleNumbers']:
@@ -173,9 +152,7 @@ class sudokuSolver:
                         square['possibleNumbers'] = [oneChanceNumber]
                         self.check_oneChanceNumber()
                         break
-            
-        print(f'Square Number: {squareNumber}')
-        print(f'Possible Numbers: {possibleNumbers}')
+
         return somethingChange
 
     def check_All3x3Square_OneChance(self):
@@ -206,7 +183,6 @@ class sudokuSolver:
         oneChanceNumbers = list()
         for possibleNumber, repetitions in possibleNumbers.items():
             if repetitions == 1:
-                print(possibleNumber, repetitions)
                 oneChanceNumbers.append(int(possibleNumber))
 
         for oneChanceNumber in oneChanceNumbers:
@@ -217,9 +193,6 @@ class sudokuSolver:
                         square['possibleNumbers'] = [oneChanceNumber]
                         self.check_oneChanceNumber()
                         break
-            
-        print(f'Row: {row}')
-        print(f'Possible Numbers: {possibleNumbers}')
         return somethingChange
 
     def check_AllLine_OneChance(self):
@@ -251,7 +224,6 @@ class sudokuSolver:
         
         for possibleNumber, repetitions in possibleNumbers.items():
             if repetitions == 1:
-                print(possibleNumber, repetitions)
                 oneChanceNumbers.append(int(possibleNumber))
         
         for oneChanceNumber in oneChanceNumbers:
@@ -262,9 +234,7 @@ class sudokuSolver:
                         square['possibleNumbers'] = [oneChanceNumber]
                         self.check_oneChanceNumber()
                         break
-            
-        print(f'column: {column}')
-        print(f'Possible Numbers: {possibleNumbers}')
+
         return somethingChange
 
     def check_AllColumn_OneChance(self):
@@ -272,6 +242,47 @@ class sudokuSolver:
             somethingChange = self.check_column_OneChance(i)
         return somethingChange
 
+    def check_singleSquare(self, row, column):
+        forbiddenNumbers = set()    
+        squareNumber = self.utilities.convertToSquareNumber(row, column)
+        
+        forbiddenNumbers.update( set(self.check_3x3Square(squareNumber)) )
+        forbiddenNumbers.update( set(self.check_Column(column)) )
+        forbiddenNumbers.update( set(self.check_Line(row)) )
+        forbiddenNumbers = list(forbiddenNumbers)    
+        
+        return forbiddenNumbers
+
+    def convertToPossibleNumbers(self, forbiddenNumbers):
+        allNumbers = {1,2,3,4,5,6,7,8,9}
+        forbiddenNumbers = set(forbiddenNumbers)
+        possibleNumbers = allNumbers.difference(forbiddenNumbers)
+            
+        return possibleNumbers
+
+    def check_oneChanceNumber(self):
+        somethingChange = False
+        for square in self.detailedTable:
+            if square["isConstant"] == False:
+                if len(square['possibleNumbers']) == 1:
+                    somethingChange = True
+                    row = square['row']
+                    column = square['column']
+                    newNumber = square['possibleNumbers'][0]
+                    self.table[row][column] = newNumber
+        return somethingChange
+    
+    def check_isFinished(self):
+        for row in self.table:
+            for square in row:
+                if square == 0:
+                    return False
+        print(f'The sudoku is solved ')
+        return self.utilities.printBeautySudoku(self.table)
+
+class sudokuUtilities:
+    def __init__(self) -> None:
+        pass
     def convertToSquareNumber(self, row, column):
         squareNumber = 0
         
@@ -298,83 +309,48 @@ class sudokuSolver:
                 squareNumber = 8
                 
         return squareNumber
-
-    def check_singleSquare(self, row, column):
-        forbiddenNumbers = set()    
-        squareNumber = self.convertToSquareNumber(row, column)
-        
-        # print(row,",",column)
-        # print(f'squareNumber: {squareNumber}')
-        forbiddenNumbers.update( set(self.check_3x3Square(squareNumber)) )
-        # print(f'3x3 square: {forbiddenNumbers}')
-        forbiddenNumbers.update( set(self.check_Column(column)) )
-        # print(f'column: {forbiddenNumbers}')
-        forbiddenNumbers.update( set(self.check_Line(row)) )
-        # print(f'line: {forbiddenNumbers}')
-        forbiddenNumbers = list(forbiddenNumbers)    
-        # print(f'{forbiddenNumbers}')
-        # print("-----------------")   
-        
-        return forbiddenNumbers
-
-    def convertToPossibleNumbers(self, forbiddenNumbers):
-        allNumbers = {1,2,3,4,5,6,7,8,9}
-        forbiddenNumbers = set(forbiddenNumbers)
-        possibleNumbers = allNumbers.difference(forbiddenNumbers)
-            
-        return possibleNumbers
-
-    def check_oneChanceNumber(self):
-        somethingChange = False
-        for square in self.detailedTable:
-            if square["isConstant"] == False:
-                if len(square['possibleNumbers']) == 1:
-                    somethingChange = True
-                    row = square['row']
-                    column = square['column']
-                    if row == 6 and column == 5:
-                        print(square)
-                    newNumber = square['possibleNumbers'][0]
-                    self.table[row][column] = newNumber
-                    # print(row, column)
-                    # print(f"is now: {newNumber}")
-        return somethingChange
     
-    def check_isFinished(self):
+    def printBeautySudoku(self, table):
+        sudokuText = ''
+        separators = [2, 5]
         for row in table:
-            for square in row:
-                if square == 0:
-                    return False
-        print(f'The sudoku is solved ')
-        self.printBeautySudoku()
-
-class tableMaker:
-    def __init__(self):
-        self.emptyTable = list()
-
-        for r in range(0,9):
-            self.row = list()
-            for c in range(0,9):
-                self.row.append(0)
-            self.emptyTable.append(self.row)
+            for c, number in enumerate(row):
+                sudokuText += str(number) + ' '
+                if c in separators:
+                    sudokuText += '\t'
+            sudokuText += '\n'
+            if table.index(row) in separators:
+                sudokuText += '\n'
+        
+        return print(sudokuText)
     
-    def addNumber(self, row, column, number):
-        self.table = self.emptyTable.copy()
-        self.table[row][column] = number               
-
-table = [
-    [0,0,5,  0,0,0,  6,0,0],
-    [3,1,0,  0,0,0,  0,2,8],
-    [0,7,0,  0,1,0,  0,4,0],
-      
-    [9,0,0,  0,2,0,  0,0,3],
-    [0,0,1,  9,3,5,  7,0,0],
-    [4,0,0,  0,7,0,  0,0,6],
-      
-    [0,9,0,  0,4,0,  0,6,0],
-    [5,3,0,  0,0,0,  0,7,2],
-    [0,0,4,  0,0,0,  9,0,0]
-]
+""" table = [
+    [0, 9, 2,    0, 0, 0,    3, 5, 0],
+    [0, 0, 0,    3, 0, 2,    0, 0, 0],
+    [0, 0, 1,    0, 7, 0,    9, 0, 0],
+    
+    [0, 4, 0,    9, 0, 3,    0, 2, 0],
+    [9, 0, 3,    0, 2, 0,    8, 0, 1],
+    [0, 7, 0,    1, 0, 8,    0, 3, 0],
+    
+    [0, 0, 7,    0, 6, 0,    1, 0, 0],
+    [0, 0, 0,    8, 0, 4,    0, 0, 0],
+    [0, 3, 5,    0, 0, 0,    4, 9, 0]
+] """
 
 if __name__ == '__main__':
-    sudolver = sudokuSolver(table)
+    table = [
+    [0, 9, 2,    0, 0, 0,    3, 5, 0],
+    [0, 0, 0,    3, 0, 2,    0, 0, 0],
+    [0, 0, 1,    0, 7, 0,    9, 0, 0],
+    
+    [0, 4, 0,    9, 0, 3,    0, 2, 0],
+    [9, 0, 3,    0, 2, 0,    8, 0, 1],
+    [0, 7, 0,    1, 0, 8,    0, 3, 0],
+    
+    [0, 0, 7,    0, 6, 0,    1, 0, 0],
+    [0, 0, 0,    8, 0, 4,    0, 0, 0],
+    [0, 3, 5,    0, 0, 0,    4, 9, 0]
+]
+    sudolver = sudokuSolver()
+    sudolver.solveSudoku(table)
---
+++ tableMaker.py
@@ -0,0 +1,160 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+from tkinter import *
+from functools import partial
+from sudoker import sudokuSolver
+
+class tableMaker(tk.Tk):
+    def __init__(self, prefabTable = None):
+        super().__init__()
+        self.title("Sudoku Board")
+        self.prefabTable = prefabTable
+        self.sudokuTable = []
+        self.formatedSudokuTable = []
+        self.cells = {}        
+        self.create_sudoku_board()
+        self.resizable(height = 0, width = 0)
+        
+        button = tk.Button(
+            master              = self,
+            text                = 'Print board',
+            font                = font.Font(size=25, weight="bold"),
+            fg                  = "black",
+            highlightbackground = "lightblue",
+            relief              = "solid",
+            command             = self.printBoard
+        )
+        
+        button2 = tk.Button(
+            master              = self,
+            text                = 'Solve sudoku',
+            font                = font.Font(size=25, weight="bold"),
+            fg                  = "black",
+            highlightbackground = "lightblue",
+            relief              = "solid",
+            command             = self.solveSudoku
+        )
+        button.pack()
+        button2.pack()
+    
+    def create_sudoku_board(self):
+        grid_frame =  tk.Frame(master=self)
+        grid_frame.pack()
+        buttonRow = 0       
+        for row in range(1,10):
+            buttonColumn = 0       
+            self.rowconfigure(row, weight=0, minsize=25)
+            self.columnconfigure(row, weight=0, minsize=25)
+            for column in range(1,10):
+                if self.prefabTable != None and self.prefabTable[row-1][column-1] != 0:
+                    number = self.prefabTable[row-1][column-1]
+                else: 
+                    number = ''
+                    
+                button = tk.Button(
+                    master = grid_frame,
+                    text = number,
+                    font = font.Font(size=25, weight="bold"),
+                    fg = "black",
+                    width = 3,
+                    height = 1,
+                    highlightbackground = "lightblue",
+                    relief = "solid",
+                    command = partial(self.increaseNumber, row,column)
+                )
+                button.bind("<Button-3>", partial(self.decreaseNumber, row, column))
+                
+                self.cells[(row-1)*9 + column] = button
+                
+                button.grid(
+                    row     = row    + buttonRow,
+                    column  = column + buttonColumn,
+                    pady    = 2,
+                    padx    = 2,
+                )
+                
+                if column % 3 == 0 and column != 9:
+                    buttonColumn += 1
+                    spacer = tk.Label(master = grid_frame, text="         ")
+                    spacer.grid(row=row, column=column + buttonColumn)    
+                    
+                if row % 3 == 0 and column == 9:
+                    buttonRow += 1               
+                    spacer = tk.Label(master = grid_frame, text=" ")
+                    spacer.grid(row=row + buttonRow, column=0)
+     
+    def updateBoard(self):
+        self.convertListToSudoku()
+        for cell in self.cells.items():
+            cell[1]['text'] = self.sudokuTable[cell[0]-1]
+                                      
+    def increaseNumber(self, row, column):
+        button = self.cells[(row-1)*9 + column]
+        
+        number = int(button['text']) + 1  if button['text'] != '' else 1
+        
+        if number >= 10:
+            number = ''
+        button['text'] = number        
+        
+    def decreaseNumber(self, row, column, useless):
+        button = self.cells[(row-1)*9 + column]
+        
+        number = int(button['text']) - 1  if button['text'] != '' else 9
+        
+        if number <= 0:
+            number = ''
+        button['text'] = number        
+
+    def convertSudokuToList(self):        
+        for row in range(9):
+            self.formatedSudokuTable.append(list())            
+        
+        row = 0
+        for i, button in enumerate(self.cells.items()):
+            if button[1]['text'] != '':
+                self.formatedSudokuTable[row].append(int(button[1]['text']))
+            else:
+                self.formatedSudokuTable[row].append(0)
+            if (i+1)% 9 == 0:
+                row += 1 
+
+    def convertListToSudoku(self):
+        for row in self.formatedSudokuTable:
+            for number in row:
+                self.sudokuTable.append(number)
+
+    def printBoard(self):
+        self.convertSudokuToList()
+        print(self.formatedSudokuTable)
+    
+    def solveSudoku(self):
+        self.convertSudokuToList()
+        print(self.formatedSudokuTable)
+        sudoku = sudokuSolver()
+        sudoku.solveSudoku(self.formatedSudokuTable)
+        self.updateBoard()
+        
+def main():
+    table =    [
+        [0, 9, 2,    0, 0, 0,    3, 5, 0],
+        [0, 0, 0,    3, 0, 2,    0, 0, 0],
+        [0, 0, 1,    0, 7, 0,    9, 0, 0],
+        
+        [0, 4, 0,    9, 0, 3,    0, 2, 0],
+        [9, 0, 3,    0, 2, 0,    8, 0, 1],
+        [0, 7, 0,    1, 0, 8,    0, 3, 0],
+        
+        [0, 0, 7,    0, 6, 0,    1, 0, 0],
+        [0, 0, 0,    8, 0, 4,    0, 0, 0],
+        [0, 3, 5,    0, 0, 0,    4, 9, 0]
+    ]
+    board = tableMaker(table)
+    board.mainloop()
+    # sudolver = sudokuSolver()
+    # sudolver.solveSudoku(table)
+
+if __name__ == '__main__':
+    main()
+    
